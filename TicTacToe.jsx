df0564530ce1ce15e7832520374fabb75def263e
Noel Rollon <noel.rollon@oocl.com>
2022-08-22 14:07:27 +0800

    Function Components

--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -1,20 +1,9 @@
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <button className="square" 
-      onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
