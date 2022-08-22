4aef135d20c89bc2773bc4165d2bc8d29774262a
Noel Rollon <noel.rollon@oocl.com>
2022-08-22 14:00:42 +0800

    Making an Interactive Component

--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -1,7 +1,14 @@
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => console.log('click')}>
         {this.props.value}
       </button>
     );
